613db292804aecbf01dc774bbe4eeab2d9b012bf
Jaredrhd <jaredrhd@gmail.com>
2020-03-01 20:27:21 +0200

    Removed Moodle From Master Branch
    Moodle is currently in it's own branch and should not be in master as of yet.

--- moodle/h5p/js/h5p_overrides.js
+++
@@ -1,9 +0,0 @@
-H5P._getLibraryPath = H5P.getLibraryPath;
-H5P.getLibraryPath = function (library) {
-    if (H5PIntegration.moodleLibraryPaths) {
-        if (H5PIntegration.moodleLibraryPaths[library]) {
-            return H5PIntegration.moodleLibraryPaths[library];
-        }
-    }
-    return H5P._getLibraryPath(library);
-};
