133e44313e29e6555e6b6136a54397f1ac5ac3a8
Jaredrhd <jaredrhd@gmail.com>
2020-03-18 14:27:44 +0200

    Deleted Server and added just plugin

---
+++ lang/en/qtype_logicgate.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_logicgate', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    qtype
+ * @subpackage logicgate
+ * @copyright  THEYEAR YOURNAME (YOURCONTACTINFO)
+
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'logicgate';
+$string['pluginname_help'] = 'Create a cloze question type with embedded response fields in the question text to enter a numeric
+or text value or select a value from a number of options.';
+$string['pluginname_link'] = 'question/type/logicgate';
+$string['pluginnameadding'] = 'Adding a logicgate question';
+$string['pluginnameediting'] = 'Editing a logicgate question';
+$string['pluginnamesummary'] = 'A logicgate question type which allows the embedding of the response fields for various available
+sub questions in the question text.
+
+So the student can enter a numeric or short text answer or choose an answer or answer(s) from
+ using a select box, check boxes or radio boxes.';
---
+++ renderer.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * logicgate question renderer class.
+ *
+ * @package    qtype
+ * @subpackage logicgate
+ * @copyright  THEYEAR YOURNAME (YOURCONTACTINFO)
+
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * Generates the output for logicgate questions.
+ *
+ * @copyright  THEYEAR YOURNAME (YOURCONTACTINFO)
+
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_logicgate_renderer extends qtype_renderer {
+
+    public function formulation_and_controls(question_attempt $qa, question_display_options $options) 
+    {
+        $question = $qa->get_question();
+
+        $questiontext = $question->format_questiontext($qa);
+        $placeholder = false;
+        if (preg_match('/_____+/', $questiontext, $matches)) {
+            $placeholder = $matches[0];
+        }
+        $input = '**subq controls go in here**';
+
+        if ($placeholder) {
+            $questiontext = substr_replace($questiontext, $input,
+                    strpos($questiontext, $placeholder), strlen($placeholder));
+        }
+
+        $result = html_writer::tag('div', $questiontext, array('class' => 'qtext'));
+
+        return $result;
+
+        return '<img  height="300" width="250" src = "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg">';
+    }
+
+    public function specific_feedback(question_attempt $qa) {
+        // TODO.
+        return 'specific_feedback';
+    }
+
+    public function correct_response(question_attempt $qa) {
+        // TODO.
+        return 'correct_response';
+    }
+}
