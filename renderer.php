d071de227b927afb18dcb0383912709d2f04dd9b
Jaredrhd <jaredrhd@gmail.com>
2020-03-19 20:33:55 +0200

    Added the code for moodle to save the file
    Added the input control to the html document.
    
    Added the necessary code to allow moodle to save and redisplay the answer after marking.

--- renderer.php
+++ renderer.php
@@ -57,15 +57,6 @@ class qtype_logicgate_renderer extends qtype_renderer
         $currentanswer = $qa->get_last_qt_var('answer');
         $inputname = $qa->get_qt_field_name('answer');
 
-        $inputattributes = array(
-            'type' => 'hidden',
-            'name' => $inputname,
-            'value' => $currentanswer,
-            'id' => $inputname,
-            'size' => 80,
-            'class' => 'answer',
-        );
-
         //If the input box is read only
         if ($options->readonly) {
             $inputattributes['readonly'] = 'readonly';
@@ -73,17 +64,6 @@ class qtype_logicgate_renderer extends qtype_renderer
 
         //Displays the red x or tick
         $feedbackimg = '';
-
-        //If we are showing their mark
-        if ($options->correctness) {
-            //Get the answer
-            $answer = $question->grade_response(array('answer' => $currentanswer));
-            $fraction = $answer[0];
-
-            //Set input field diabled and set tick or cross
-            $inputattributes['class'] .= ' ' . $this->feedback_class($fraction);
-            $feedbackimg = $this->feedback_image($fraction);
-        }
 
         //Get the file
         $input = file_get_contents(new moodle_url('/question/type/logicgate/Lab.html')) . $feedbackimg;
@@ -96,7 +76,27 @@ class qtype_logicgate_renderer extends qtype_renderer
             $result .= html_writer::tag('span', $input, array('class' => 'answer'));
         }
 
-        //TODO get the input field form and edit its name attribute to $inputname
+        //Get the string pos at ANSWER_NAME_ID
+        $position = strpos($result,"ANSWER_NAME_ID",0);
+
+        //Get the value= position from the last position, this is the value we need to edit to show answer 
+        $position = strpos($result,"value=",$position);
+
+        //Place answer at the calculated position
+        $result = substr_replace($result, $currentanswer . '"', $position+7);
+
+        //Replace the ANSWER_NAME_ID with the question id for saving
+        $result = str_replace("ANSWER_NAME_ID", $inputname, $result);
+
+        //If we are showing their mark
+        if ($options->correctness) {
+            //Get the answer
+            $answer = $question->grade_response(array('answer' => $currentanswer));
+            $fraction = $answer[0];
+
+            //Set input field diabled and set tick or cross
+            $feedbackimg = $this->feedback_image($fraction);
+        }        
 
         //return result
         return $result;
