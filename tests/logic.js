28a298964c1f378955d5e455742ed24506e466a5
Jonah <jonah.alter@gmail.com>
2020-05-03 18:21:56 +0200

    Added NAND logic gate test

--- tests/logic.js
+++ tests/logic.js
@@ -10,4 +10,10 @@ function and(a, b){
     return 0;
 }
 
-module.exports = { not, and };
+function nand(a, b){
+    if (a == 1 && b == 1)
+        return 0;
+    return 1;  
+}
+
+module.exports = { not, and, nand };
--- tests/logic.test.js
+++ tests/logic.test.js
@@ -1,4 +1,4 @@
-const { not, and } = require("./logic.js");
+const { not, and, nand } = require("./logic.js");
 
 test("Takes in 1 input and applies NOT gate logic", () => {
     expect(not(0)).toBe(1);
@@ -10,4 +10,11 @@ test("Takes in 2 inputs and applies AND gate logic", () => {
     expect(and(0, 1)).toBe(0);
     expect(and(1, 0)).toBe(0);
     expect(and(1, 1)).toBe(1);
+});
+
+test("Takes in 2 inputs and applies NAND gate logic", () => {
+    expect(nand(0, 0)).toBe(1);
+    expect(nand(0, 1)).toBe(1);
+    expect(nand(1, 0)).toBe(1);
+    expect(nand(1, 1)).toBe(0);
 });
