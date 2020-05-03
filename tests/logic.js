0f1e15254cedcf4adb21d4bed95ce5ad1bf81751
Max <max.parkinn@gmail.com>
2020-05-03 18:38:18 +0200

    NOR logic gate test

--- tests/logic.js
+++ tests/logic.js
@@ -22,8 +22,14 @@ function or(a, b) {
     return 0;
 }
 
+function nor(a, b) {
+    if(a == 0 && b == 0)
+        return 1;
+    return 0;
+}
+
 function pythagDistance(p1, p2) {
     return Math.sqrt( Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2) );
 }
 
-module.exports = { not, and, nand, or, pythagDistance };
+module.exports = { not, and, nand, or, nor, pythagDistance };
--- tests/logic.test.js
+++ tests/logic.test.js
@@ -1,4 +1,4 @@
-const { not, and, nand, or, pythagDistance } = require("./logic.js");
+const { not, and, nand, or, nor, pythagDistance } = require("./logic.js");
 
 test("Takes in 1 input and applies NOT gate logic", () => {
     expect(not(0)).toBe(1);
@@ -24,6 +24,13 @@ test("Takes in 2 inputs and applies OR gate", () => {
     expect(or(1, 0)).toBe(1);
     expect(or(0, 1)).toBe(1);
     expect(or(1, 1)).toBe(1);
+});
+
+test("Takes in 2 inputs and applies NOR gate", () => {
+    expect(nor(0, 0)).toBe(1);
+    expect(nor(1, 0)).toBe(0);
+    expect(nor(0, 1)).toBe(0);
+    expect(nor(1, 1)).toBe(0);
 });
 
 test("Takes in 2 points and calculates the Pythagorean distance", () => {
