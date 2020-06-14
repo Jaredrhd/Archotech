e4e9cc416138f710fe1fdf60bccfa948bcdc628b
Jonah <jonah.alter@gmail.com>
2020-06-14 20:05:06 +0200

    XOR NAND Integration Test

--- tests/logic.js
+++ tests/logic.js
@@ -108,6 +108,10 @@ function xnorNor(a, b, c) {
     return nor(xnor(a, b), c);
 }
 
+function xorNand(a, b, c) {
+    return nand(xor(a, b), c);
+}
+
 function checkOrthogonalAfterSum(v1, v2, v3, v4) {
     let u = {x: addVectors(v1, v2)[0], y: addVectors(v1, v2)[1]};
     let v = {x: addVectors(v3, v4)[0], y: addVectors(v3, v4)[1]};
@@ -118,4 +122,4 @@ function checkOrthogonalAfterSum(v1, v2, v3, v4) {
 module.exports = { not, and, nand, or, nor, xor, xnor, manhattanDistance, 
                    pythagDistance, midpoint, vectorMagnitude, addVectors, dotProduct, dotProductTheta,
                    minkowskiDistance, findDirectionVector, subtractVectors, calculateAngle, orthogonalVectors, xorAnd, 
-                   xnorAnd, xorNor, xnorNor, checkOrthogonalAfterSum };
+                   xnorAnd, xorNor, xnorNor, xorNand, checkOrthogonalAfterSum };
--- tests/logic.test.js
+++ tests/logic.test.js
@@ -1,7 +1,7 @@
 const { not, and, nand, or, nor, xor, xnor, manhattanDistance, 
         pythagDistance, midpoint, vectorMagnitude, addVectors, dotProduct, dotProductTheta,
         minkowskiDistance, findDirectionVector, subtractVectors, calculateAngle, orthogonalVectors,
-        xorAnd, xnorAnd, xorNor, xnorNor, checkOrthogonalAfterSum } = require("./logic.js");
+        xorAnd, xnorAnd, xorNor, xnorNor, xorNand, checkOrthogonalAfterSum } = require("./logic.js");
 
 test("Takes in 1 input and applies NOT gate logic", () => {
     expect(not(0)).toBe(1);
@@ -145,6 +145,12 @@ test("Takes in 3 inputs, applies xnor gate to first two and nor gate to final ex
     expect(xnorNor(1, 0, 0)).toBe(1);
     expect(xnorNor(1, 1, 0)).toBe(0);
     expect(xnorNor(0, 0, 1)).toBe(0);
+});
+
+test("Takes in 3 inputs, applies xor gate to first two and nand gate to final expression", () => {
+    expect(xorNand(0, 1, 1)).toBe(0);
+    expect(xorNand(1, 1, 1)).toBe(1);
+    expect(xorNand(1, 1, 0)).toBe(1);
 });
 
 test("Takes in 4 vectors, sums pairs of vectors and checks their orthogonality after summing", () => {
