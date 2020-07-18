7b1e4a0ef14ea440dcd8a95393fca210ef09db45
Max <max.parkinn@gmail.com>
2020-07-18 19:52:55 +0200

    Added tree foundation + functionality

--- trees/js/Node.js
+++ trees/js/Node.js
@@ -1,29 +1,98 @@
-import {TransformedObject, treeNode} from "./main.js";
-
 class Node {
-    constructor(value) {
+    constructor(value, root = true) {
         this._value = value;
-        this._outline = new TransformedObject(treeNode);
+        this._isRoot = root;
+        this._cellCoords = {x: null, y: null};
+        this._boardCoords = null;
+        this._nodeRadius = 0;
+        this._selected = false;
+        this._parent = null;
         this._children = {leftChild: null, rightChild: null};
     }
 
     get value() {
         return this._value;
-    }   
+    }
+    
+    get isRoot() {
+        return this._isRoot;
+    }
 
-    get outline() {
-        return this._outline;
+    get cellCoords() {
+        return this._cellCoords;
+    }
+
+    get boardCoords() {
+        return this._boardCoords;
+    }
+
+    get nodeRadius() {
+        return this._nodeRadius;
+    }
+
+    get selected() {
+        return this._selected;
+    }
+
+    get parent() {
+        return this._parent;
     }
 
     get children() {
         return this._children;
     }
 
-    set leftChild(node) {
-        this._children.leftChild = node;
+    set selected(value) {
+        this._selected = value;
+    }
+    
+    set parent(node) {
+        this._parent = node;
     }
 
-    set rightChild(node) {
-        this._children.rightChild = node;
+    /** Draws a node on the canvas */
+    draw(board, parent, cellX, cellY) { 
+        board.context.save();
+        
+        board.context.lineWidth = 2;
+        board.context.beginPath();
+
+        this._cellCoords.x = cellX;
+        this._cellCoords.y = cellY;
+        this._boardCoords = board.cellToBoardCoords(this._cellCoords.x, this._cellCoords.y);
+        this._nodeRadius = board.cellWidth * 0.45;
+
+        board.context.arc(this._boardCoords.x, this._boardCoords.y, this._nodeRadius, 0, 2*Math.PI); // Draw node circle
+
+        board.context.font = "30px Arial";
+        board.context.textAlign = "center"; 
+        board.context.textBaseline = "middle";
+        board.context.fillText(this._value, this._boardCoords.x, this._boardCoords.y); // Draw node value
+
+        if(this._selected) {
+            board.context.strokeStyle = "red";
+        }
+
+        board.context.stroke();
+
+        board.context.restore();
+
+        if(parent !== null) { // Draw the connecting edge from the parent to the node
+            let angleParent = this._parent.children.leftChild === this ? (150 * Math.PI / 180) : (30 * Math.PI / 180); // Parent has larger angle if child is left child
+            let angleChild = this._parent.children.leftChild === this ? (325 * Math.PI / 180) : (215 * Math.PI / 180); // Child has larger angle if child is left child
+
+            board.context.save();
+
+            board.context.lineWidth = 2;
+            board.context.beginPath();
+
+            /** Calculate the end points of the edge on the nodes' circumferences (x = cx + r*cos(angle), y = cy + r*sin(angle)) */
+            board.context.moveTo(this._nodeRadius*Math.cos(angleParent) + parent.boardCoords.x, this._nodeRadius*Math.sin(angleParent) + parent.boardCoords.y);
+            board.context.lineTo(this._nodeRadius*Math.cos(angleChild) + this._boardCoords.x, this._nodeRadius*Math.sin(angleChild) + this._boardCoords.y);
+
+            board.context.stroke();
+
+            board.context.restore();
+        }
     }
 }
--- trees/js/Tree.js
+++ trees/js/Tree.js
@@ -1,13 +1,41 @@
 class Tree {
-    constructor(rootValue) {
+    constructor(rootValue, board) {
+        // Create matrix for nodes
+        this._nodes = new Array(board.rows);
+        for(let i = 0; i < this._nodes.length; i++) {
+            this._nodes[i] = new Array(board.columns);
+        }
+
         this._root = new Node(rootValue);
+        this._root.parent = null;
+        this._root.draw(board, null, (board.columns - 1) * 0.5, 0); // board, parent, cellX, cellY
+        this._nodes[this._root.cellCoords.y][this._root.cellCoords.x] = this._root;
     }
 
-    addLeftChild(selectedNode, childValue) {
-        selectedNode.leftChild = new Node(childValue);
+    get root() {
+        return this._root;
     }
 
-    addRightChild(selectedNode, childValue) {
-        selectedNode.rightChild = new Node(childValue);
+    get nodes() {
+        return this._nodes;
     }
+
+    addChild(selectedNode, board, childType, childValue, childCellX, childCellY) {
+        let newChild;
+
+        if(childType === "left") {
+            selectedNode.children.leftChild = new Node(childValue, false);
+            selectedNode.children.leftChild.parent = selectedNode;
+            newChild = selectedNode.children.leftChild;
+        }
+        else {
+            selectedNode.children.rightChild = new Node(childValue, false);
+            selectedNode.children.rightChild.parent = selectedNode;
+            newChild = selectedNode.children.rightChild;
+        }
+
+        newChild.draw(board, selectedNode, childCellX, childCellY);
+        this._nodes[childCellY][childCellX] = newChild;
+    }
+
 }
